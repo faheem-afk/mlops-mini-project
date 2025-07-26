b63cc39723b0f5e4fdf5e5d593ad9f463031f21f
= <adahm7114@gmail.com>
2025-07-26 09:27:04 +0100

    ss

--- src/data/data_preprocessing.py
+++ src/data/data_preprocessing.py
@@ -4,7 +4,6 @@ import numpy as np
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import pandas as pd
-import nltk
 
 train_df = pd.read_csv("data/interim/train_data.csv")
 test_df = pd.read_csv("data/interim/test_data.csv")
