b941d26834700f18bd59ab142dbfa2261e83f4b8
= <adahm7114@gmail.com>
2025-07-26 07:18:03 +0100

    added ci

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -3,10 +3,11 @@ import os
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 import warnings
+import yaml
 
 warnings.filterwarnings('ignore')
-# max_features = yaml.safe_load(
-# open('params.yaml', 'r'))['feature_engineering']['max_features']
+max_features = yaml.safe_load(
+    open('params.yaml', 'r'))['feature_engineering']['max_features']
 train_preprocessed_df = pd.read_csv(
     "data/preprocessed/train_preprocessed_data.csv").dropna(axis=0)
 test_preprocessed_df = pd.read_csv(
@@ -16,7 +17,7 @@ y_train = train_preprocessed_df['sentiment'].values
 X_test = test_preprocessed_df['content'].values
 y_test = test_preprocessed_df['sentiment'].values
 # Apply Bag of Words (CountVectorizer)
-vectorizer = CountVectorizer()
+vectorizer = CountVectorizer(max_features=max_features)
 # Fit the vectorizer on the training data and transform it
 X_train_bow = vectorizer.fit_transform(X_train)
 # Transform the test data using the same vectorizer
