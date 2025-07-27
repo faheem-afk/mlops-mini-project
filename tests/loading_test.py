ca41a023475bcbf7d571d4c98281c287d6709718
= <adahm7114@gmail.com>
2025-07-27 01:08:14 +0100

    signature test

--- tests/loading_test.py
+++ tests/loading_test.py
@@ -6,6 +6,8 @@ import warnings
 from dotenv import load_dotenv
 import pandas as pd
 import joblib
+from sklearn.metrics import \
+    accuracy_score, precision_score, f1_score, recall_score
 
 load_dotenv()
 
@@ -19,16 +21,19 @@ class TestModelLoading(unittest.TestCase):
         os.environ['MLFLOW_TRACKING_PASSWORD'] = dagshub_token
         mlflow.set_tracking_uri(
             "https://dagshub.com/faheem-afk/mlops-mini-project.mlflow")
+
         model_name = 'logisticRegression'
         model_info = json.load(open('reports/experiment_info.json', 'r'))
         model_uri = f"runs:/{model_info['run_id']}/{model_name}"
         model_uri = f"runs:/{model_info['run_id']}/{model_name}"
         cls.model = mlflow.sklearn.load_model(model_uri)
+
         artifact_uri_ = f"""
         runs:/{model_info['run_id']}/vectorizer/vectorizer.joblib"""
         local_path = mlflow.artifacts. \
             download_artifacts(artifact_uri=artifact_uri_)
         cls.vectorizer = joblib.load(local_path)
+
         return (
             (cls.model, cls.vectorizer) if
             (cls.model and cls.vectorizer) else None)
@@ -39,13 +44,37 @@ class TestModelLoading(unittest.TestCase):
     def test_model_signature(self):
         input_text = 'hi how are you'
         input_data_df = pd.DataFrame([[input_text]], columns=['content'])
+
         input_transformed = self.vectorizer.transform(
             input_data_df['content'].tolist())
+
         prediction = self.model.predict(input_transformed)
+
         self.assertEqual(input_transformed.shape[1],
                          len(self.vectorizer.get_feature_names_out()))
-        self.assertEqual(len(prediction), input_data_df.shape.shape[0])
+        self.assertEqual(len(prediction), input_data_df.shape[0])
         self.assertEqual(len(prediction), 1)
+
+    def test_model(self):
+        X_holdout = self.holdout_data.iloc[:, 0:-1]
+        y_hold = self.hold_data.iloc[:, -1]
+
+        y_pred_new = self.new_model.predict(X_holdout)
+
+        accuracy_new = accuracy_score(y_hold, y_pred_new)
+        precision_new = precision_score(y_hold, y_pred_new)
+        recall_new = recall_score(y_hold, y_pred_new)
+        f1_new = f1_score(y_hold, y_pred_new)
+
+        expected_accuracy = 0.70
+        expected_precision = 0.70
+        expected_recall = 0.70
+        expected_f1 = 0.70
+
+        self.assertGreaterEqual(accuracy_new, expected_accuracy)
+        self.assertGreaterEqual(precision_new, expected_precision)
+        self.assertGreaterEqual(recall_new, expected_recall)
+        self.assertGreaterEqual(f1_new, expected_f1)
 
 
 if __name__ == "__main__":
