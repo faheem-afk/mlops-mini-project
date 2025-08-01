3a6aeb1c91d06b9e02ff9d3966584989f43238e4
= <adahm7114@gmail.com>
2025-08-01 20:33:31 +0100

    v3

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# login into aws ecr
-
+# Login to ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 596514762357.dkr.ecr.us-east-1.amazonaws.com
+
+# Pull the Docker image
 docker pull 596514762357.dkr.ecr.us-east-1.amazonaws.com/mlops-mini-project:v3
 
-if ["$(docker ps -q -f name=my-container)"]; then
+# If a container with the name 'my-container' is running or exists, stop and remove it
+if [ "$(docker ps -aq -f name=my-container)" ]; then
     docker stop my-container
     docker rm my-container
 fi
 
+# Run the container
 docker run -d -p 80:5000 --name my-container 596514762357.dkr.ecr.us-east-1.amazonaws.com/mlops-mini-project:v3
