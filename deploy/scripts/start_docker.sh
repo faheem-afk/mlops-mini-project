d919e4f03a63d9351942d0d6f25c852ccca1d3e7
= <adahm7114@gmail.com>
2025-08-01 20:22:36 +0100

    add v3

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -3,7 +3,11 @@
 # login into aws ecr
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 596514762357.dkr.ecr.us-east-1.amazonaws.com
-docker pull 596514762357.dkr.ecr.us-east-1.amazonaws.com/mlops-mini-project:latest
-docker stop my-container || true
-docker rm my-container || true
-docker run -d -p 80:5000 --name my-container 596514762357.dkr.ecr.us-east-1.amazonaws.com/mlops-mini-project:latest
+docker pull 596514762357.dkr.ecr.us-east-1.amazonaws.com/mlops-mini-project:v3
+
+if ["$(docker ps -q -f name=my-container)"]; then
+    docker stop my-container
+    docker rm my-container
+fi
+
+docker run -d -p 80:5000 --name my-container 596514762357.dkr.ecr.us-east-1.amazonaws.com/mlops-mini-project:v3
